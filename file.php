74117a81b88f642181009574a8de6ee8e9a71021
nimikkiko0 <109760250+nimikkiko0@users.noreply.github.com>
2022-07-31 20:49:22 +0530

    Add files via upload

---
+++ file.php
@@ -0,0 +1,147 @@
+
+<?php
+/**
+ * Example bot.
+ *
+ * Copyright 2016-2020 Daniil Gentili
+ * (https://daniil.it)
+ * This file is part of MadelineProto.
+ * MadelineProto is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * MadelineProto is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with MadelineProto.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Daniil Gentili <daniil@daniil.it>
+ * @copyright 2016-2020 Daniil Gentili <daniil@daniil.it>
+ * @license   https://opensource.org/licenses/AGPL-3.0 AGPLv3
+ *
+ * @link https://docs.madelineproto.xyz MadelineProto documentation
+ */
+
+use danog\MadelineProto\Db\DbArray;
+use danog\MadelineProto\EventHandler;
+use danog\MadelineProto\Logger;
+use danog\MadelineProto\Settings;
+use danog\MadelineProto\Settings\Database\Mysql;
+use danog\MadelineProto\Settings\Database\Postgres;
+use danog\MadelineProto\Settings\Database\Redis;
+
+/*
+ * Various ways to load MadelineProto
+ */
+if (\file_exists('vendor/autoload.php')) {
+    include 'vendor/autoload.php';
+} else {
+    if (!\file_exists('madeline.php')) {
+        \copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+    }
+    /**
+     * @psalm-suppress MissingFile
+     */
+    include 'madeline.php';
+}
+
+/**
+ * Event handler class.
+ */
+class MyEventHandler extends EventHandler
+{
+    /**
+     * @var int|string Username or ID of bot admin
+     */
+    const ADMIN = "shckers"; // Change this
+
+    /**
+     * List of properties automatically stored in database (MySQL, Postgres, redis or memory).
+     * @see https://docs.madelineproto.xyz/docs/DATABASE.html
+     * @var array
+     */
+
+    /**
+     * @var DbArray<array>
+     */
+    protected $dataStoredOnDb;
+
+    /**
+     * Get peer(s) where to report errors.
+     *
+     * @return int|string|array
+     */
+    public function getReportPeers()
+    {
+        return [self::ADMIN];
+    }
+    /**
+     * Initialization logic.
+     */
+    public function onStart()
+    {
+        $this->logger("The bot was started!");
+        \var_dump(yield $this->getFullInfo('madelineproto'));
+    }
+    /**
+     * Handle updates from supergroups and channels.
+     *
+     * @param array $update Update
+     *
+     * @return void
+     */
+    public function onUpdateNewChannelMessage(array $update): \Generator
+    {
+        return $this->onUpdateNewMessage($update);
+    }
+
+    /**
+     * Handle updates from users.
+     *
+     * @param array $update Update
+     *
+     * @return \Generator
+     */
+    public function onUpdateNewMessage(array $update): \Generator
+    {
+        if ($update['message']['_'] === 'messageEmpty' || $update['message']['out'] ?? false) {
+            return;
+        }
+
+    
+
+   
+        $filename = yield $this->downloadToDir($update['message'] , __DIR__ . '/tmp/');
+        
+		
+		$filenames =  str_replace(" ", "\\ ", $filename);
+
+            // ffmpeg -y -i %1 -i logo.png -filter_complex overlay=x=10:y=H-h-10 output.mp4
+            shell_exec("ffmpeg -y -i ".$filenames. " -i logo.png -filter_complex overlay=x=10:y=H-h-10 output.mp4");
+	
+
+        	shell_exec("ffmpeg -y -i ".$filenames. " -ss 00:01:80.000 -vframes 1 thumb.png");
+
+
+        yield $this->messages->sendMedia([
+            'peer' => '-1001645403038',
+            'media' => [
+                '_' => 'inputMediaUploadedDocument',
+                'file' => 'output.mp4',
+                'thumb' => 'thumb.png',
+                'attributes' => [
+                    ['_' => 'documentAttributeVideo', 'round_message' => true, 'supports_streaming' => true]    
+                ]
+            ],
+            'message' => '[BACKUP](https://t.me/latinnas)',
+            'parse_mode' => 'Markdown'
+        ]);
+	    
+	  unlink($filenames);
+
+
+        
+    }
+}
+
+$settings = new Settings;
+$settings->getLogger()->setLevel(Logger::LEVEL_ULTRA_VERBOSE);
+
+MyEventHandler::startAndLoop('uwu.madeline', $settings);
